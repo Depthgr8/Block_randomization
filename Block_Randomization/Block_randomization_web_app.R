656026b49e46947d6544c84efc8b347c8c05521b
Depthgr8 <depthgr8@gmail.com>
2017-03-19 02:12:23 -0700

    Display table on web app

--- Block_Randomization/Block_randomization_web_app.R
+++ Block_Randomization/Block_randomization_web_app.R
@@ -1,53 +1,62 @@
-# This script is the source for a web app to plot block randomization
+# Documentation -----------------------------------------------------------
+
+# Web app to produce block randomization trials
+# Author: Deepak Sharma
+# Computer Programmer, JPNATC - AIIMS
+# Date: 28 March, 2017
+
+# Import libraries --------------------------------------------------------
 
 library(shiny)
 library(psych)
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Block randomization in R"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("trials",
-                     "Number of trials:",
-                     min = 1,
-                     max = 300,
-                     value = 150),
-         sliderInput("blocks",
-                     "Number of blocks:",
-                     min = 1,
-                     max = 10,
-                     value = 3),
-         sliderInput("cases",
-                     "Number of cases:",
-                     min = 1,
-                     max = 4,
-                     value = 2)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x <- input$blocks
-      y <- input$trials
-      condition <-  block.random( n= input$trials,c(block = input$blocks,drug = 2))
-      # draw the histogram with the specified number of bins
-      plot(condition[,2], condition[,3], col = 'darkgray', border = 'white')
-   })
+# User interface ----------------------------------------------------------
+
+ui <- fluidPage(
+  titlePanel("Block randomization in R"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        "trials",
+        "Number of trials:",
+        min = 1,
+        max = 300,
+        value = 150
+      ),
+      sliderInput(
+        "blocks",
+        "Number of blocks:",
+        min = 1,
+        max = 10,
+        value = 3
+      ),
+      sliderInput(
+        "cases",
+        "Number of cases:",
+        min = 1,
+        max = 4,
+        value = 2
+      )
+    ),
+    
+    # Show result table
+    mainPanel(tableOutput("filetable"))
+  ))
+
+
+# Server ------------------------------------------------------------------
+
+server <- function(input, output)
+{
+  output$filetable <- renderTable({
+    condition <-
+      block.random(n = input$trials, c(block = input$blocks, drug = input$cases))
+    output <- data.frame(condition)
+    colnames(output) <- c("Block", "to_be_discarded", "Case")
+    output[,-2]
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run application ---------------------------------------------------------
 
+shinyApp(ui = ui, server = server)
--- block_randomization.r
+++ block_randomization.r
@@ -1,8 +1,23 @@
-install.packages("psych")
+# Include psych package
+
 library(psych)
 
-condition <-  block.random( n= 150,c(block = 3,drug = 2))
-headTail(condition)
-write.csv(condition,"randomized_design.csv")
-pairs.panels(condition)
-plot(condition,type = 'l')
+# Generate N number of trials ---------------------------------------------
+
+n <- 150
+not_useful <- 3
+cases <- 2
+
+# Generate randomized trials for N = 150 ----------------------------------
+
+trials <-  block.random(n,c(not_useful, cases))
+headTail(trials)
+output <- data.frame(trials)
+colnames(output) <- c("Block no.","to_be_discarded","Case")
+
+# Write CSV file ----------------------------------------------------------
+
+# Discarding second column which contains non-relevent data
+write.csv(output[,-2],"Randomized_trials.csv")
+pairs.panels(trials)
+plot(trials,type = 'l')
