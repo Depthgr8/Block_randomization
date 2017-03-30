abce0e210e1aa2902d889c0683c00c3d30bc1e4a
Depthgr8 <depthgr8@gmail.com>
2017-03-30 15:18:07 +0530

    Added export functionality (.csv, .txt, .tsv) in web app

--- Block_Randomization/Block_randomization_web_app.R
+++ Block_Randomization/Block_randomization_web_app.R
@@ -1,6 +1,6 @@
 # Documentation -----------------------------------------------------------
 
-# Web app to produce block randomization trials
+# Shiny web app to produce block randomization trials
 # Author: Deepak Sharma
 # Computer Programmer, JPNATC - AIIMS
 # Date: 28 March, 2017
@@ -13,59 +13,47 @@ library(psych)
 # User interface ----------------------------------------------------------
 
 ui <- fluidPage(
-  titlePanel("Block randomization in R"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput(
-        "trials",
-        "Number of trials:",
-        min = 1,
-        max = 300,
-        value = 150
-      ),
-      sliderInput(
-        "blocks",
-        "Number of blocks:",
-        min = 1,
-        max = 10,
-        value = 3
-      ),
-      sliderInput(
-        "cases",
-        "Number of cases:",
-        min = 1,
-        max = 4,
-        value = 2
-      ),
-      downloadButton('downloadData', 'Download')
-    ),
-    
-    # Show result table
-    mainPanel(tableOutput("filetable"))
-    
-  ))
-
+  titlePanel('Block randomization in R'),
+    sidebarLayout(
+      sidebarPanel(
+          h3("Instructions"),
+          h6("For 150 trials enter trials = 150"),
+          h6("Factor = Desired block trials / number of cases"),
+          h6("Example: To produce a block of 6 trials"),
+          h6("Enter factor = 3 and cases = 2, since 3 * 2 = 6 "),
+          textInput("trials", "Number of trials ","150"),
+          textInput("factor", "Number of factor ","3"),
+          textInput("cases", "Number of cases ","2"),
+          radioButtons("filetype", "Save as ", choices = c("txt", "csv", "tsv")),
+          downloadButton('trials', 'Download trials')
+          ),
+          mainPanel(tableOutput('table')
+      )
+    )
+)
 
 # Server ------------------------------------------------------------------
 
-server <- function(input, output)
-{
-  output$filetable <- renderTable({
-    condition <-
-      block.random(n = input$trials, c(block = input$blocks, drug = input$cases))
+server <- function(input, output){
+  datasetInput <- reactive({
+    condition <- block.random(as.numeric(input$trials), c(as.numeric(input$factor), as.numeric(input$cases)))
     output <- data.frame(condition)
     colnames(output) <- c("Block", "to_be_discarded", "Case")
     data <- output[,-2]
+    })
+    output$table <- renderTable({
+      datasetInput()
   })
-  data <- mtcars
-  output$table <- renderTable({
-    datasetInput()
-  })
-  output$downloadData <- downloadHandler(
+
+  output$trials <- downloadHandler(
     filename = function() {
-      paste("trials", '.txt', sep='')},
-    content = function (file){
-      write.csv(data,file)
+      paste(input$filetype, sep = ".")
+    },
+  
+# Write content in file    
+  content = function(file) {
+    sep <- switch(input$filetype, "csv" = ",", "tsv" = "\t","txt" = " ")
+    write.table(datasetInput(), file, sep = sep, row.names = FALSE)
     }
   )
 }
